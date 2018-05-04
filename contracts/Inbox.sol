fed28038809b7dd3d59ed83d0dd1a9247c7f9989
Soumyadeep m.soumyadeep@gmail.com <Soumyadeep m.soumyadeep@gmail.com>
2018-05-04 23:51:31 +0200

    initial commit

---
+++ contracts/Inbox.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.0;
+
+contract Inbox {
+
+    string public message;
+    
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+    
+    function getMessage() public view returns (string) {
+        return message;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+}
---
+++ test/Inbox.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+	//get a list of alll acounts
+	accounts = await web3.eth.getAccounts();
+	
+	//use one of the accounts to deploy the contract
+	inbox = await new web3.eth.Contract(JSON.parse(interface))
+						.deploy({ data: bytecode, arguments: ['Hi There'] })
+						.send({ from: accounts[0], gas: '1000000' });
+});
+
+describe('Inbox', () => {
+	it('deploys a contract', () => {
+		assert.ok(inbox.options.address);		
+	});
+	it('initial value is set', async () => {
+		const message = await inbox.methods.message().call();
+		assert.ok(message, 'Hi There');
+	});
+	it('can change message', async () => {
+		await inbox.methods.setMessage('bye').send({from: accounts[0]});
+		const message = await inbox.methods.message().call();
+		assert.ok(message, 'bye');
+	});
+});
